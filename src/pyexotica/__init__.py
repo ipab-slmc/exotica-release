da8b23dce067ddcc26422965e56ec7d53a2cbd12
Wolfgang Merkt <w.merkt@gmail.com>
2020-01-31 19:41:09 +0000

    Rebase from 'debian/melodic/exotica_python'

--- src/pyexotica/__init__.py
+++ src/pyexotica/__init__.py
@@ -1,3 +1,11 @@
-from _pyexotica import *
-import publish_trajectory
-# import planning_scene_utils # pyassimp import currently fails on Kinetic, will fix
+from __future__ import absolute_import
+
+from ._pyexotica import *
+from .publish_trajectory import *
+from .tools import *
+from .interactive_cost_tuning import *
+from .jupyter_meshcat import *
+
+# Used for backwards compatibility only, deprecated
+Visualization = VisualizationMoveIt
+# from .planning_scene_utils import * # pyassimp import currently fails on Kinetic, will fix
