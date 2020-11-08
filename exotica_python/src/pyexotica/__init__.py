c982677454479069d70834c097f6c5b12a626309
Wolfgang Merkt <w.merkt@gmail.com>
2020-11-08 23:54:47 +0000

    Rebase from 'upstream'

--- exotica_python/src/pyexotica/__init__.py
+++ exotica_python/src/pyexotica/__init__.py
@@ -5,7 +5,7 @@ from .publish_trajectory import *
 from .tools import *
 from .interactive_cost_tuning import *
 from .jupyter_meshcat import *
+from .testing import *
+from .transformations import *
 
-# Used for backwards compatibility only, deprecated
-Visualization = VisualizationMoveIt
 # from .planning_scene_utils import * # pyassimp import currently fails on Kinetic, will fix
